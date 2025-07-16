f90cd0b975b09a510003c14edd4c64ba4c36f982
Hassan Osama <hassanusamacs@gmail.com>
2025-07-17 00:44:22 +0300

    initial commit

---
+++ user-service/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log('🚀 Server running on http://localhost:3000');
+});
